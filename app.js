c04a92bc9687cc7b9c42c9e55e6125c431b979e9
tramluvmy <quoctram3051@gmail.com>
2024-12-22 22:01:27 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const yourDate = new Date("2024-09-28T04:46:00"),
-music = ['ido', 'noinaycoanh', 'nguoiamphu'];
+music = ['christmas','ido', 'noinaycoanh', 'nguoiamphu'];
 
 document.addEventListener('DOMContentLoaded', function(){
       var rootTime = document.querySelector("time");
@@ -16,7 +16,8 @@ document.addEventListener('DOMContentLoaded', function(){
             rootTime.textContent = `${(hrs>9)?hrs:"0"+hrs}:${(min>9)?min:"0"+min}:${(sec>9)?sec:"0"+sec}`;
       } olock();
       var timer = setInterval(function(){olock()}, 1000);
-      document.querySelector("audio").setAttribute("src", `music/${music[Math.floor(Math.random()*music.length)]}.mp3`);
+      // document.querySelector("audio").setAttribute("src", `music/${music[Math.floor(Math.random()*music.length)]}.mp3`);
+      document.querySelector("audio").setAttribute("src", `music/${music[0]}.mp3`);
 
       document.getElementsByTagName("body")[0].insertAdjacentHTML(
             "beforeend",
